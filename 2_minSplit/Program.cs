7d8021a6b291b0f0691e137a3e334a3d07036d19
grandiosa iva <ggo24@yahoo.com>
2020-10-14 14:27:34 +0400

    2_minSplit

---
+++ 2_minSplit/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_minSplit
+{
+    class Program
+    {
+        static int sum = 0;
+        static int divisor1 = 50, divisor2 = 20, divisor3 = 10, divisor4 = 5, divisor5 = 1;
+        static void Main(string[] args)
+        {
+            int result = minSplit(120);
+            Console.ReadKey();
+        }
+        static int minSplit(int amount)
+        {
+            while (amount / divisor1 >= 1)
+            {
+                sum++;
+                amount -= divisor1;
+            }
+            while (amount / divisor2 >= 1)
+            {
+                sum++;
+                amount -= divisor2;
+            }
+            while (amount / divisor3 >= 1)
+            {
+                sum++;
+                amount -= divisor3;
+            }
+            while (amount / divisor4 >= 1)
+            {
+                sum++;
+                amount -= divisor4;
+            }
+            while (amount / divisor5 >= 1)
+            {
+                sum++;
+                amount -= divisor5;
+            }
+            return sum;
+        }
+    }
+}
---
+++ 2_minSplit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("2_minSplit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_minSplit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58d24da9-bcd2-4955-aeb7-948f61bad43b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
