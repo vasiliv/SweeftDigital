02694a3fb345497d60996abf3cad9d05601844b8
grandiosa iva <ggo24@yahoo.com>
2020-10-14 13:35:09 +0400

    Add project files.

---
+++ 1_isPalindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_isPalindrome
+{
+    class Program    {
+        public static bool answer;
+
+        static void Main(string[] args)
+        {
+            bool result = isPalindrome("abba");
+            Console.ReadLine();
+        }
+        static Boolean isPalindrome(String text)
+        {
+            
+            for (int i = 0; i <  text.Length/2; i++)
+            {
+                if (text[i] == text[text.Length - 1 - i])
+                {
+                    answer = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            return answer;
+        }
+    }
+}
